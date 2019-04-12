ce9a617e217a0bd610aa357841a6b2878ec2da9e
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-11 19:53:31 -0500

    Finished creating account schema

--- models/Account.js
+++ models/Account.js
@@ -2,5 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AccountSchema = new Schema({
-  
-})
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  accessToken: {
+    type: String,
+    require: true
+  },
+  itemId: {
+    type: String,
+    require: true
+  },
+  institutionName: {
+    type: String,
+    require: true
+  },
+  accountName: {
+    type: String,
+
+  },
+  accountType: {
+    type: String,
+
+  },
+  accountSubType: {
+    type: String,    
+  }
+});
+
+module.exports = Account = mongoose.model("account", AccountSchema);
