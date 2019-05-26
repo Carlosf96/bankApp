3be97781f2dd7c0fedfbe1a300c034bbc6d61c36
Carlosf96 <carlos.f.roque96@gmail.com>
2019-05-26 13:00:03 -0500

    Finished adding account/transactions reducer

--- client/src/reducers/accountReducers.js
+++ client/src/reducers/accountReducers.js
@@ -12,5 +12,45 @@ const inititialState = {
   transactions: [],
   accountsLoading: false,
   transactionsLoading: false
-}
+};
 
+export default function(state = inititialState, action) {
+  switch (action.type) {
+    case ACCOUNTS_LOADING:
+      return {
+        ...state,
+        accountsLoading: true
+      };
+    case ADD_ACCOUNT:
+      return {
+        ...state,
+        accounts: [action.payload, ...state.accounts]
+      };
+    case DELETE_ACCOUNT:
+      return {
+        ...state,
+        accounts: state.accounts.filter(
+          account => account._id !== action.payload
+        )
+      };
+    case GET_ACCOUNTS:
+      return {
+        ...state,
+        accounts: action.payload,
+        accountsLoading: false
+      };
+    case TRANSACTIONS_LOADING:
+      return {
+        ...state,
+        transactionsLoading: true
+      };
+    case GET_TRANSACTIONS:
+      return {
+        ...state,
+        transactions: action.payload,
+        transactionsLoading: false
+      };
+    default:
+      return state;
+  }
+}
