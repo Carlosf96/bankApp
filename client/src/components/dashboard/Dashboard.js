9cf2d16cbcc4a6c47fde6dfbd710c8aff757e5d4
Carlosf96 <carlos.f.roque96@gmail.com>
2019-05-26 13:49:39 -0500

    Finished renovating dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -75,43 +75,23 @@ class Dashboard extends Component {
       );
     }
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👻
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
+      <div className="container">{dashboardContent}</div>
     );
   }
 }
 
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  getAccounts: PropTypes.func.isRequired,
+  addAccount: PropTypes.func.isRequired,
+  plaid: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  plaid: state.plaid
 });
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, getAccounts, addAccount }
 )(Dashboard);
